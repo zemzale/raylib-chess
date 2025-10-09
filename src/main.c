eed18f69ef8a00b73cbf112eb4a3008a47d907c1
Alberts Zemzale <alberts.zemzale@gmail.com>
2025-10-09 19:09:52 +0300

    Draw the chess board

--- src/main.c
+++ src/main.c
@@ -3,55 +3,73 @@ Raylib example file.
 This is an example main file for a simple raylib project.
 Use this as a starting point or replace it with your code.
 
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
+by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit
+https://creativecommons.org/publicdomain/zero/1.0/
 
 */
 
 #include "raylib.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h" // utility header for SearchAndSetResourceDir
+//
+const int BOARD_WIDTH = 64 * 8;
 
-void DrawChessboard()
-{
-	DrawRectangle(100, 100, 64 * 8, 64 * 8, WHITE);
-	// TODO: Finish up drawing this chessboard
+void DrawChessboard() {
+  bool isWhite = true;
+  Color sqrColor = WHITE;
+  Vector2 pos = {.x = 64, .y = 64};
+  Vector2 size = {.x = 64, .y = 64};
+
+  int offsetX = GetScreenWidth() / 2 - BOARD_WIDTH / 2;
+  int offsetY = GetScreenHeight() / 2 - BOARD_WIDTH / 2;
+
+  for (int x = 0; x < 8; x++) {
+    for (int y = 0; y < 8; y++) {
+      pos.x = offsetX + (64 * x);
+      pos.y = offsetY + (64 * y);
+      DrawRectangleV(pos, size, isWhite ? WHITE : BLACK);
+      isWhite = !isWhite;
+    }
+    isWhite = !isWhite;
+  }
 }
 
-int main ()
-{
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+int main() {
+  // Tell the window to use vsync and work on high DPI displays
+  SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+  // Create the window and OpenGL context
+  InitWindow(1280, 800, "Hello Raylib");
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+  // Utility function from resource_dir.h to find the resources folder and set
+  // it as the current working directory so we can load from it
+  SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-	{
-		// drawing
-		BeginDrawing();
+  // Load a texture from the resources directory
+  Texture wabbit = LoadTexture("wabbit_alpha.png");
 
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+  // game loop
+  while (!WindowShouldClose()) // run the loop untill the user presses ESCAPE or
+                               // presses the Close button on the window
+  {
+    // drawing
+    BeginDrawing();
 
-		DrawChessboard();
+    // Setup the back buffer for drawing (clear color and depth buffers)
+    ClearBackground(BLACK);
 
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-		EndDrawing();
-	}
+    DrawChessboard();
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+    // end the frame and get ready for the next one  (display frame, poll input,
+    // etc...)
+    EndDrawing();
+  }
 
-	// destroy the window and cleanup the OpenGL context
-	CloseWindow();
-	return 0;
+  // cleanup
+  // unload our texture so it can be cleaned up
+  UnloadTexture(wabbit);
+
+  // destroy the window and cleanup the OpenGL context
+  CloseWindow();
+  return 0;
 }
